b2a7cd7309e28880def80fdb0b586514243d2efc
陽贝 <1850773150@qq.com>
2025-04-21 21:07:00 +0800

    转义字符题

---
+++ 转义字符/zhuanyizifu.java
@@ -0,0 +1,10 @@
+public class zhuanyizifu{
+	public static void main(String[] args){
+		System.out.println("北京\t上海\t天津");
+		System.out.println("jack\nsmith\nmary");
+		System.out.println("C:\\Windows\\System32\\cmd.exe");
+		System.out.println("老韩说：\"要好好学习\"");
+		System.out.println("韩顺平教育\r北京");
+		System.out.println("韩顺平教育\r\n北京");
+	}
+}
