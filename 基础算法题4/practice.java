da2272a9d9e022db6c4630a56bb2251735897929
陽贝 <1850773150@qq.com>
2025-04-24 19:25:21 +0800

    JAVA的一些基础算法题

---
+++ 基础算法题4/practice.java
@@ -0,0 +1,26 @@
+public class practice{
+	public static void main(String[] args){
+		Book book = new Book("笑傲江湖",300);
+		book.info();
+		book.updatePrice();
+		book.info();
+	}
+}
+class Book{
+	String name;
+	double price;
+	public Book(String name,double price){
+		this.name = name;
+		this.price = price;
+	}
+	public void updatePrice(){
+		if(this.price > 150){
+			this.price = 150;
+		}else if(this.price > 100){
+			this.price = 100;
+		}
+	}
+	public void info(){
+		System.out.println("书名=" + this.name + " " + "价格=" + this.price);
+	}
+}
---
+++ 基础算法题4/tian.java
@@ -0,0 +1,8 @@
+public class tian{
+	public static void main(String[] args){
+		int n = 59;
+		int x = 59 / 7;
+		int t = 59 % 7;
+		System.out.println(n + "天  合" + x + "星期" + t + "天");
+	}
+}
